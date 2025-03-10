b4458ed05922f823101ae977cc66e3a8caa35d7b
foclabroc <foclabroc@hotmail.fr>
2025-03-10 12:06:07 +0100

    Fix "ports" updater

--- roms/ports/Switch Updater.sh
+++ roms/ports/Switch Updater.sh
@@ -7,7 +7,7 @@
 #####################################################################
 updater=/userdata/system/switch/extra/batocera-switch-updater.sh
 rm "$updater" 2>/dev/null 
-wget -q --no-check-certificate --no-cache --no-cookies -O "$updater" "https://raw.githubusercontent.com/foclabroc/batocera-switch/main/system/switch/extra/batocera-switch-updater.sh"
+wget -q --no-check-certificate --no-cache --no-cookies -O "$updater" "https://raw.githubusercontent.com/foclabroc/batocera-switch/refs/heads/42/system/switch/extra/batocera-switch-updater.sh"
 dos2unix "$updater"
 chmod a+x "$updater"
 bash /userdata/system/switch/extra/batocera-switch-updater.sh
