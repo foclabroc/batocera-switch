99304b3564bd04acab4d5cf07abefeeec62cf144
foclabroc <foclabroc@hotmail.fr>
2025-03-13 09:04:02 +0100

    add universal commande line

--- system/switch/extra/switch-install.sh
+++ system/switch/extra/switch-install.sh
@@ -5,15 +5,15 @@ version=$(batocera-es-swissknife --version | awk '{print $1}' | sed -E 's/^([0-9
 
 # Compare version and decide which download to trigger
 if (( version == 39 || version == 40 )); then
-    echo "Batocera version is 39 or 40. Triggering download for version 39/40..."
+    echo "[Foclabroc Switch Installer] Batocera version is 39 or 40. Triggering download for version 39/40..."
     sleep 5
     curl -L bit.ly/foclabroc-switchoff-40 | bash
 elif (( version == 41 )); then
-    echo "Batocera version is 41. Triggering download for version 41+l..."
+    echo "[Foclabroc Switch Installer] Batocera version is 41. Triggering download for version 41..."
     sleep 5
     curl -L bit.ly/foclabroc-switchoff | bash
 elif (( version == 42 )); then
-    echo "Batocera version is 42. Triggering download for version 42..."
+    echo "[Foclabroc Switch Installer] Batocera version is 42. Triggering download for version 42+..."
     sleep 5
     curl -L bit.ly/foclabroc-batswitch | bash
 else
