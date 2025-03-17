56149426618aaf1267bf050aa197a1fe99eae373
foclabroc <foclabroc@hotmail.fr>
2025-03-17 17:10:44 +0100

    add press enter message

--- system/switch/extra/batocera-switch-installer-v40.sh
+++ system/switch/extra/batocera-switch-installer-v40.sh
@@ -380,7 +380,12 @@ echo -e "   ${X}> emulators logs are in /userdata/system/switch/logs/${X}"
 echo -e "   ${X}> emulationstation logs are in /userdata/system/logs/${X}" 
 echo -e "   ${X}-------------------------------------------------------------------${X}"
 echo 
+echo -e "   ${GREEN}RELOAD YOUR GAMELIST AND ENJOY${GREEN}" 
 echo 
+echo -e "   ${GREEN}Installation Completed. Press Enter to exit...${GREEN}"
+echo -e "   ${GREEN}Installation Terminée. Appuyez sur Entrée pour quitter...${X}"
+read -r  # Attend que l'utilisateur appuie sur Entrée
+exit 0
 else
 clear 
 echo 
