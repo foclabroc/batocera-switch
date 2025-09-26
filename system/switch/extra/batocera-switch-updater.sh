c34eab36c85d8e0e8b6d4ecff153204532cb317b
foclabroc <foclabroc@hotmail.fr>
2025-09-26 10:01:29 +0200

    ...

--- system/switch/extra/batocera-switch-updater.sh
+++ system/switch/extra/batocera-switch-updater.sh
@@ -891,12 +891,13 @@ T=$THEME_COLOR_RYUJINX
 	# fi
 rm /userdata/system/switch/appimages/ryujinx-1.2.81-linux_x64.tar.gz 2>/dev/null
 rm /userdata/system/switch/appimages/ryujinx-1.2.96-linux_x64.tar.gz 2>/dev/null
-# version=$(
-  # curl -s "https://git.ryujinx.app/ryubing/canary/-/releases" |
-  # grep -oP '/ryubing/ryujinx/-/releases/\K[0-9]+\.[0-9]+\.[0-9]+' |
-  # head -n1
-# )
-version="1.3.138"
+version=$(
+  curl -s "https://git.ryujinx.app/ryubing/canary/-/releases" |
+  grep -oP '/ryubing/canary/-/releases/[0-9]+\.[0-9]+\.[0-9]+' |
+  sed -E 's#.*/##' |
+  head -n1
+)
+#version="1.3.138"
 wget -q --show-progress --tries=10 --no-check-certificate --no-cache --no-cookies -O "/userdata/system/switch/ryujinx-${version}-linux_x64.tar.gz" "https://git.ryujinx.app/api/v4/projects/68/packages/generic/Ryubing-Canary/${version}/ryujinx-canary-${version}-linux_x64.tar.gz"
 link_ryujinx=/userdata/system/switch/ryujinx-${version}-linux_x64.tar.gz
 link_tarR="/userdata/system/switch/appimages/ryujinx-canary-1.3.138-linux_x64.tar.gz"
